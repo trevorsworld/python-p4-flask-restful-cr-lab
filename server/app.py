34e9460645c43a21ba032052ebf405cb413b02d4
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-07-02 09:01:15 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -16,11 +16,37 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.route('/')
+def index():
+    return "<h1> PLANTS API </h1>"
+
 class Plants(Resource):
-    pass
+    def get (self):
+        plants_dict = [plant.to_dict() for plant in Plant.query.all()]
+        resp = make_response (jsonify(plants_dict),200)
+        return resp
+    
+    def post (self):
+        new_plant = Plant (
+            name = request.json['name'],
+            image = request.json.get('image'),
+            price = request.json.get('price')
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        new_plant_dict = new_plant.to_dict()
+        resp = make_response(jsonify(new_plant_dict) , 201)
+        return resp
+    #pass
 
 class PlantByID(Resource):
-    pass
+    def get (self, id):
+        plant_dict = Plant.query.filter(Plant.id == id) .first().to_dict()
+        return make_response (jsonify(plant_dict), 200)
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID,'/plants/<int:id>')    
+    #pass
         
 
 if __name__ == '__main__':
